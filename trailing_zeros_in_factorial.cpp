007c60dff13dd3bb85eb65f71f742822bbf8b456
MaryamNizam <52219900+MaryamNizam@users.noreply.github.com>
2019-08-31 12:16:18 +0500

    Add files via upload

---
+++ trailing_zeros_in_factorial.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int number;
+    int n=0;
+    cout<<"enter number\n";
+    cin>>number;
+    
+    for(int divisor=5; divisor<=number; divisor=divisor*5)
+        n=n+number/divisor;
+    
+    
+    
+    cout<<"number of trailing zeros in "<<number<<"! are = "<<n<<endl;
+    
+    system("pause");
+    return 0;
+}
